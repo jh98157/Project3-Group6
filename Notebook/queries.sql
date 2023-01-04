43164df7f75a8c559523c79a34586633fbb20a4b
mitchboullt <112483018+mitchboullt@users.noreply.github.com>
2023-01-03 23:09:24 -0600

    intial dataset, table query, and csv upload

---
+++ Notebook/queries.sql
@@ -0,0 +1,30 @@
+CREATE TABLE haunted (
+rank INT PRIMARY KEY,
+city text,
+country text,
+description text,
+location text,
+state text,
+longitude bigint,
+latitude bigint
+);
+
+CREATE TABLE ufos (
+summary text,
+country text,
+city text,
+state text,
+shape text,
+date_time date,
+longitude bigint,
+latitude bigint
+);
+
+CREATE TABLE bigfoot (
+title text,
+classificatioin text,
+timestamp date,
+longitude bigint,
+latitude bigint
+);
+
