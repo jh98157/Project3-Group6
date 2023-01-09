56d722e931ebc8112be6a9ed40a03954da67e917
mitchboullt <112483018+mitchboullt@users.noreply.github.com>
2023-01-09 17:31:59 -0600

    html templates, flask app doc, and status js rough draft upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/map/")
+def data():
+    return render_template('map.html')
+@app.route("/bystate/")
+def comparison():
+    return render_template('bargraph.html')
+@app.route("/overtime/")
+def cloudiness():
+    return render_template('linegraph.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
